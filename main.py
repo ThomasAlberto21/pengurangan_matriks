72c34e4713f300d914fb2447a696daa21844df6f
ThomasAlberto21 <thomasalberto456@gmail.com>
2022-11-10 18:56:49 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+print("\nNama Anggota Kelompok :")
+print("Thomas Alberto (12221355)")
+print("Fransiskus Berto (12221358)")
+print("Yulia (12221350)")
+print("Lisana (12221080)")
+print("Narko(12221357)")
+print("\n")
+
+
+print("MATRIKS 1 :")
+matriks_1 = np.array([[5, 9, 8], [4, 3, 2], [8, 4, 2]])
+print(matriks_1)
+
+print("\n")
+
+print("MATRIKS 2 :")
+matriks_2 = np.array([[1, 4, 6], [3, 3, 5], [5, 6, 7]])
+
+print(matriks_2)
+
+print("\n")
+
+
+print("HASIL PENGURANGAN MATRIKS : ")
+hasil = np.subtract(matriks_1, matriks_2)
+print(hasil)
+
+print("\n")
